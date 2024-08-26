be4fd5bf3bff67c04dc9eddad8da091135f68449
이희주 <hanu@neighbor21.co.kr>
2024-08-26 17:19:50 +0900

    [FEATURE]CORS 설정

--- src/main/java/com/hanu/jpa_backend/Controller.java
+++ src/main/java/com/hanu/jpa_backend/Controller.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://43.201.132.91:13799")
 public class Controller {
 
     @GetMapping("/api/test")
